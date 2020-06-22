30dbb0f18cc261ee82ca73079820c81b23782ba7
kenkeeeenbbt <kenkeeeenbbt@gmail.com>
2020-06-22 15:53:31 +0900

    Usersコントローラの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_17_102927) do
+ActiveRecord::Schema.define(version: 2020_06_20_031837) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.text "content"
-    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
