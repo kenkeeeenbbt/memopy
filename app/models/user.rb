5f394d01710b94ed1c1560f8f297a7e03c4d0f26
kenkeeeenbbt <kenkeeeenbbt@gmail.com>
2020-06-22 20:00:20 +0900

    文字数の検証

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-    validates :name, presence: true
-    validates :email, presence: true
+    validates :name, presence: true, length: { maximum: 20 }
+    validates :email, presence: true, length: { maximum: 255 }
     has_secure_password
-    validates :password, presence: true
+    validates :password, presence: true, length: { minimum: 8 }
 end
