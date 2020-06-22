1d01b516ee1bed27b4150d80955ad611ec720dd5
kenkeeeenbbt <kenkeeeenbbt@gmail.com>
2020-06-22 18:29:42 +0900

    バリデーション、存在性の検証

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+
+    before do
+        @user = build(:user)
+    end
+
+    describe '存在性の検証' do
+        it '名前がない場合、無効であること' do
+            @user.name = ' '
+            expect(@user).not_to be_valid
+        end
+    
+        it 'メールアドレスがない場合、無効であること' do
+            @user.email = ' '
+            expect(@user).not_to be_valid
+        end
+    end
 end
