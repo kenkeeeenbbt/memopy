e18ff578ea4badd917a7b71cee79c1b9cfca2d69
kenkeeeenbbt <kenkeeeenbbt@gmail.com>
2020-06-22 20:00:20 +0900

    パスワードダイジェストの追加
    パスワードの追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,21 @@ RSpec.describe User, type: :model do
         @user = build(:user)
     end
 
+    describe '有効性の検証' do
+        it '有効なファクトリを持つこと' do
+            expect(build(:user)).to be_valid
+        end
+    
+        it '名前、メール、パスワードがある場合、有効であること' do
+            user = User.new(
+                name: 'TestUser',
+                email: 'test@expample.com',
+                password: 'password'
+            )
+            expect(user).to be_valid
+        end
+    end
+
     describe '存在性の検証' do
         it '名前がない場合、無効であること' do
             @user.name = ' '
@@ -14,6 +29,11 @@ RSpec.describe User, type: :model do
     
         it 'メールアドレスがない場合、無効であること' do
             @user.email = ' '
+            expect(@user).not_to be_valid
+        end
+
+        it 'パスワードがない場合、無効であること' do
+            @user.password = @user.password_confirmation = ' ' * 8
             expect(@user).not_to be_valid
         end
     end
