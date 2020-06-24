5b8f27877f5f6efb7461b1d8dc484728a263b804
kenkeeeenbbt <kenkeeeenbbt@gmail.com>
2020-06-24 16:44:17 +0900

    修正2

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   root 'users#new'
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
-  get '/users/:id', to: 'users#show'
   resources :users
 end
