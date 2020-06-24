2edcaa7dde2441001c3d63a6fa2b6d759ce1cb95
kenkeeeenbbt <kenkeeeenbbt@gmail.com>
2020-06-24 11:49:58 +0900

    新規登録機能の修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
   end
@@ -6,9 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = "登録しました"
-      redirect_to @user
+      redirect_to("/users/#{@user.id}") 
     else
       render 'new'
     end
