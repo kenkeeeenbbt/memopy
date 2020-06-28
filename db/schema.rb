e9cd82b3be554ae42cc9fc09c47074eea8a3b035
ma22npk <gunjo64@icloud.com>
2020-06-29 00:15:11 +0900

    files changedから破棄してしまったファイル（自体は）復元。 しかしPendingMigrationError発生。。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_22_105712) do
+ActiveRecord::Schema.define(version: 2020_06_25_044306) do
+
+  create_table "friends", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_friends_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -20,4 +28,5 @@ ActiveRecord::Schema.define(version: 2020_06_22_105712) do
     t.string "password_digest"
   end
 
+  add_foreign_key "friends", "users"
 end
