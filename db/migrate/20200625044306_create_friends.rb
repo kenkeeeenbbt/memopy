5c56cec4755cbabd586a08f20d4f65a3d700e22d
ma22npk <gunjo64@icloud.com>
2020-06-28 22:26:59 +0900

    破棄してしまったファイルの復元を手動で行なっています

---
+++ db/migrate/20200625044306_create_friends.rb
@@ -0,0 +1,10 @@
+class CreateFriends < ActiveRecord::Migration[5.2]
+  def change
+    create_table :friends do |t|
+      t.string :name
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
