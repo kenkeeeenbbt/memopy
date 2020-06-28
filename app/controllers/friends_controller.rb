ec72e4c4e99afb08af6b4cb69cad813328fddb8d
ma22npk <gunjo64@icloud.com>
2020-06-28 21:23:39 +0900

    ファイルごと破棄してしまったファイルを手動で復元中です。

---
+++ app/controllers/friends_controller.rb
@@ -0,0 +1,18 @@
+class FriendsController < ApplicationController
+  before_action :current_user
+
+  def new
+    @friend = Friend.new
+  end
+
+  def create
+    @friend = Friend.new(friend_params)
+    @friend.save
+  end
+
+  private
+
+    def friend_params
+      params.require(:friend).permit(:name, :user_id)
+    end
+end 
