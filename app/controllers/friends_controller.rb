eacc2f2275fbf757f7c3c1e55e5fb2c1dcca3144
kenkeeeenbbt <kenkeeeenbbt@gmail.com>
2020-06-30 00:41:04 +0900

    人物一覧、詳細、削除

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,5 +1,13 @@
 class FriendsController < ApplicationController
   before_action :current_user
+  before_action :set_friend, only: [:show, :destroy]
+
+  def index
+    @friends = Friend.all
+  end
+
+  def show
+  end
 
   def new
     @friend = Friend.new
@@ -8,11 +16,21 @@ class FriendsController < ApplicationController
   def create
     @friend = Friend.new(friend_params)
     @friend.save
+    redirect_to friends_url
+  end
+
+  def destroy
+    @friend.destroy
+    redirect_to friends_url
   end
 
   private
 
     def friend_params
       params.require(:friend).permit(:name, :user_id)
+    end
+
+    def set_friend
+      @friend = Friend.find(params[:id])
     end
 end
