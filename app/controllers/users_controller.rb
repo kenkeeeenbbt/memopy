b38877fc4d62e3ee2005ac849eb4b7b4a5842af7
kenkeeeenbbt <kenkeeeenbbt@gmail.com>
2020-07-03 00:01:42 +0900

    他ユーザーへのページにアクセス制御

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-  before_action :correct_user, only: [:show]
 
   def show
     @user = User.find(params[:id])
@@ -25,14 +24,5 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
-    end
-
-    # 正しいユーザーかどうかを確認
-    def correct_user
-      @user = User.find(params[:id])
-      unless @user == current_user
-        flash[:danger] = "ログインしてください。"
-        redirect_to login_url
-      end
     end
 end
