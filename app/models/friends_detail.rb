ee8d6493e14f3cd8a98c6aa86ed3c489874513c1
kenkeeeenbbt <kenkeeeenbbt@gmail.com>
2020-07-04 02:34:52 +0900

    FriendとFriendsDetailの存在性、文字数の検証

--- app/models/friends_detail.rb
+++ app/models/friends_detail.rb
@@ -1,3 +1,5 @@
 class FriendsDetail < ApplicationRecord
   belongs_to :friend
+  validates :feature, presence: true, length: { maximum: 20 }
+  validates :content, presence: true, length: { maximum: 140 }
 end
--- spec/models/friends_detail_spec.rb
+++ spec/models/friends_detail_spec.rb
@@ -8,4 +8,38 @@ RSpec.describe FriendsDetail, type: :model do
             expect(friends_detail).to be_valid
         end
     end
+
+    describe '存在性の検証' do
+        it 'featureがない場合、無効であること' do
+            friends_detail.feature = ''
+            expect(friends_detail).not_to be_valid
+        end
+
+        it 'contentがない場合、無効であること' do
+            friends_detail.content = ''
+            expect(friends_detail).not_to be_valid
+        end
+    end
+
+    describe '文字数の検証' do
+        it 'featureが20文字以内の場合、有効であること' do
+            friends_detail.feature = 'a' * 20
+            expect(friends_detail).to be_valid
+        end
+    
+        it 'featureが21文字以上の場合、無効であること' do
+            friends_detail.feature = 'a' * 21
+            expect(friends_detail).not_to be_valid
+        end
+
+        it 'contentが140文字以内の場合、有効であること' do
+            friends_detail.content = 'a' * 140
+            expect(friends_detail).to be_valid
+        end
+    
+        it 'contentが141文字以上の場合、無効であること' do
+            friends_detail.content = 'a' * 141
+            expect(friends_detail).not_to be_valid
+        end
+    end
 end
